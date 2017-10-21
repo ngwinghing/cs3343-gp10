f57a7294c4a687ec572d051eff593d0e44d23521
ngwinghing <wing.nwh@gmail.com>
2017-10-21 12:46:08 +0800

    player name

--- RummikubGame/src/rummikub/Player.java
+++ RummikubGame/src/rummikub/Player.java
@@ -3,5 +3,13 @@ package rummikub;
 public class Player {
 	String name;
 	Rack rack;
+	
+	public Player(String name) {
+		this.name = name;
+	}
 
+	public String getName() {
+		return name;
+	}
+	
 }
--- RummikubGame/src/rummikub/RummikubGame.java
+++ RummikubGame/src/rummikub/RummikubGame.java
@@ -2,4 +2,7 @@ package rummikub;
 
 public class RummikubGame {
 	
+	public static void main(String args[]) {
+        System.out.println("?");
+    }
 }
